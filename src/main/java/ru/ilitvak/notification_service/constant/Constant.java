5631a10b71b0f9a173309dd74064cb822fdd6ded
Litvak Ivan <litvakivan@gmail.com>
2025-08-24 12:48:33 +0300

    add new get /notifications/count

--- src/main/java/ru/ilitvak/notification_service/constant/Constant.java
+++ src/main/java/ru/ilitvak/notification_service/constant/Constant.java
@@ -1,5 +1,7 @@
 package ru.ilitvak.notification_service.constant;
 
 public class Constant {
-    public static String DEFAULT_LOCATION_CODE = "en";
+    public static String DEFAULT_LOCATION_CODE = "ru";
+    public static String EN_LOCATION_CODE = "en";
+    public static String RU_LOCATION_CODE = "ru";
 }
